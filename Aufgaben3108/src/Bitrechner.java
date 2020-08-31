77d4dd61365b677741e7bfff0dd37eef8cbbea9e
Milan <60037272+milan-th@users.noreply.github.com>
2020-08-31 21:00:26 +0200

    Bitrechner vereinfacht und aufpoliert

--- Aufgaben3108/src/Bitrechner.java
+++ Aufgaben3108/src/Bitrechner.java
@@ -2,20 +2,26 @@ import java.util.Scanner;
 
 public class Bitrechner {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int zahl0, zahl1, zahl2, zahl3, zahl4, zahl5, zahl7;
         double dezimal=0;
         int zähler=7;
-        int[] binary = new int[8]; //Nur möglich für 8 Stellen lange Binärezahl --> Startet bei 8. Stelle
+        int[] binary = new int[8]; //Nur möglich für 8 Stellen lange Binärezahl --> Startet höhster (8.) Stelle
+        boolean wrongInput = false;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Gebe deine Binär Zahl in einzelnen Stellen an: ");
+
         for (int i = 0; i < 8; i++){
             do {
                 binary[i] = sc.nextInt();
-            }while (binary[i] != 0 && binary[i] != 1); //Testet ob Eingabe richtig
-            if(binary[i] == 1)
-                dezimal = dezimal + Math.pow(2, zähler); //gibt Zahl einen Wert
-
+                wrongInput = false;
+                if (binary[i] != 0 && binary[i] != 1){ //Testet ob Eingabe richtig
+                    System.out.println("Das binäre Zahlensystem besteht nur aus 0 und 1 wiederhole die Eingabe.");
+                    wrongInput = true;
+                }
+            }while(wrongInput); //Wiederholt Eingabe wenn >wrongInput< true
+            if(binary[i] == 1){dezimal = dezimal + Math.pow(2, zähler);} //gibt Zahl einen Wert
             zähler--;
-            //System.out.println(binary[i] + "  "+ dezimal+ "  " + zähler);
         }
         System.out.println((int)dezimal);
 
